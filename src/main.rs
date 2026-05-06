29d00c8709ce43c3c8d0eaa6bae829b3b1ee9ecb
534791@student.saxion.nl <534791@student.saxion.nl>
2026-05-07 00:24:21 +0200

    Kindoff works

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![no_std]
+#![feature(asm_experimental_arch)]
 #![no_main]
 
-use core::{ptr::{read_volatile, write_volatile}};
+use core::{arch::asm, ptr::{read_volatile, write_volatile}};
 
 use arduino_hal::{delay_ms, delay_us};
 use panic_halt as _;
 
 const DDRD : *mut u8 = 0x2A as *mut u8; // PORT D DIRECTION REGISTER
-const PIND : *mut u8 = 0x29 as *mut u8; // PORT D INPUT DATA REGISTER
 const PORTD : *mut u8 = 0x2B as *mut u8; // PORT D OUTPUT DATA REGISTER
 const TIFR0 : *mut u8 = 0x35 as *mut u8; // TIMER COUNTER 0 INTERRUPT FLAG REGISTER
 const OCR0A : *mut u8 = 0x47 as *mut u8; // OUTPUT COMPARE REIGSTER A
@@ -22,17 +22,12 @@ const WGM01_BIT : u8 = 1 << 1;
 const TCCR0B : *mut u8 = 0x45 as *mut u8; // TC0 CONTROL REGISTER B
 const WGM02_BIT : u8 = 1 << 3;
 
-const PMX0 : *mut u8 = 0xEE as *mut u8; // Port Multiplexed Control Register
-const C0AC0_BIT : u8 = 1 << 3;
-
 const OCR0B : *mut u8 = 0x48 as *mut u8; // TC0 Output compare register B
 
-const COM0A1_BIT : u8 = 1 << 7;
 const PIN5 : u8 = 1 << 5;
 const PIN6 : u8 = 1 << 6;
 const TOV0_BIT : u8 = 1 << 0;
 const CS00_BIT : u8 = 1 << 0;
-const CS01_BIT : u8 = 1 << 1;
 
 const TCNT0 : *mut u8 = 0x46 as *mut u8; // TC0 count value register
 
@@ -45,12 +40,12 @@ const TCNT0 : *mut u8 = 0x46 as *mut u8; // TC0 count value register
 // 0.8 uS = 26 cycles
 // 0.4 uS = 13 cycles
 
-const PWM_VAL_0: u8 = 0; // ~0.4us high
-const PWM_VAL_1: u8 = 4; // ~0.8us high
+const PWM_VAL_0: u8 = 1; // ~0.4us high
+const PWM_VAL_1: u8 = 5; // ~0.8us high
 
 fn pin_off(pin : u8) {
     unsafe {
-        let mut portd_val = read_volatile(PORTD);
+        let portd_val = read_volatile(PORTD);
         write_volatile(PORTD, portd_val & !pin);
     }
 }
@@ -69,47 +64,65 @@ fn pin_on(pin : u8) {
     }
 }
 
-struct WS2812B<const BUF_SIZE: usize> {
-    buf : [u8; BUF_SIZE]
+struct WS2812B<const LED_COUNT : usize> {
+    leds : [u32; LED_COUNT],
 }
 
-impl<const BUF_SIZE : usize> WS2812B<BUF_SIZE> {
+struct LED {
+    r : u8,
+    g : u8,
+    b : u8
+}
+
+impl Into<u32> for LED {
+    fn into(self) -> u32 {
+        let value = ((self.g as u32) << 16) | ((self.r as u32) << 8) | (self.b as u32);
+        let mut value_rev = 0;
+
+        value
+    }
+}
+impl <const LED_COUNT : usize> WS2812B<LED_COUNT> {
     fn new() -> Self {
-        WS2812B {buf : [PWM_VAL_0; BUF_SIZE]}
+        WS2812B {leds : [0; LED_COUNT]}
     }
 
-    fn set_led(&mut self, index: usize, r : u8, g : u8, b : u8) {
-        let bits = ((g as u32) << 16) | ((r as u32) << 8) | (b as u32);
-        for i in 0..23 {
-            let bit = (bits >> (23 - i)) & 1; 
-            if bit == 1 {
-                self.buf[index * 24 + i] = PWM_VAL_1;
-            } else {
-                self.buf[index * 24 + i] = PWM_VAL_0;
-            }
-        }
+    fn set_led(&mut self, index : usize, value : u32) {
+        self.leds[index] = value;
     }
 
     fn update(&self) {
+        let pwm_vals = [PWM_VAL_0, PWM_VAL_1];
         unsafe {
+            asm!("cli");
             write_volatile(PORTD, read_volatile(PORTD) & !PIN5);
             write_volatile(TCCR0A, read_volatile(TCCR0A) & !COM0B1_BIT);
-            write_volatile(OCR0A, 39);
-
-            write_volatile(OCR0B, PWM_VAL_0);
+            write_volatile(OCR0A, 42); // TOP VALUE
 
             write_volatile(TCNT0, 0);
 
             write_volatile(TIFR0, TOV0_BIT); 
+
+            let arr_size = self.leds.len();
+            let mut i = 0;
+
+            
             write_volatile(TCCR0A, read_volatile(TCCR0A) | COM0B1_BIT);
+            for i in 0..LED_COUNT {
+                let mut val = self.leds[i];
+                for j in 0..24 {
+                    let pwm_val = (val & 0b10000000000000000000000 != 0) as usize;
+                    val <<= 1;
+                    
+                    while read_volatile(TIFR0) & TOV0_BIT == 0 {}
+                    write_volatile(TIFR0, TOV0_BIT);
 
-            for i in 0..24*64 {
-                let val = self.buf[i];
+                    write_volatile(OCR0B, pwm_vals[pwm_val]);
 
-                write_volatile(OCR0B, val);
-                while read_volatile(TIFR0) & TOV0_BIT == 0 {}
-                write_volatile(TIFR0, TOV0_BIT);
+                }
             }
+
+            asm!("sei");
         }
     }
 
@@ -117,14 +130,13 @@ impl<const BUF_SIZE : usize> WS2812B<BUF_SIZE> {
         unsafe {
             while read_volatile(TIFR0) & TOV0_BIT == 0 {}
 
-            let mut tccr = read_volatile(TCCR0A);
+            let tccr = read_volatile(TCCR0A);
             write_volatile(TCCR0A, tccr & !COM0B1_BIT);
 
-            let mut portd = read_volatile(PORTD);
+            let portd = read_volatile(PORTD);
             write_volatile(PORTD, portd & !PIN5);
         }
-        
-        delay_us(60);
+        delay_us(80);
     }
 }
 
@@ -153,30 +165,39 @@ macro_rules! new_ws2812b {
     };
 }
 
+const LEDS_COUNT : usize = 64;
+
 #[arduino_hal::entry]
 fn main() -> ! {
-    static mut TEST : usize = 0;
+    let mut counter : usize = 0;
 
     setup_gpio();
 
-    let mut ws2812b = WS2812B::<1536>::new();
-    loop {
-        for i in 0..64 {
-            let offset = *TEST % 2;
-            if (i + offset) % 2 == 0 {
-                ws2812b.set_led(i, 20, 0, 0);
-            } else {
-                ws2812b.set_led(i, 0, 0, 20);
-            }
-        }
+    let mut leds  = WS2812B::<LEDS_COUNT>::new();
 
-        ws2812b.update();
-        ws2812b.reset();
-        
-        delay_ms(50);
+    let mut counter = 0;
+    loop {
+        let led : LED;
+        // leds.leds = [0; LEDS_COUNT];
+        if counter < LEDS_COUNT {
+            led = LED { r: 0b10, g: 0, b: 0 };
+        }
+        else if counter < LEDS_COUNT * 2 {
+            led = LED { r: 0, g: 0b10, b: 0 };
+        } else if counter < LEDS_COUNT * 3 {
+            led = LED { r: 0, g: 0, b: 0b10 };
+        } else {
+            led = LED { r: 0b10, g: 0, b: 0 };
+            counter = 0;
+        }
+        leds.set_led(counter % LEDS_COUNT, led.into());
+        leds.update();
+        leds.reset();
+
 
         pin_toggle(PIN6);
-        
-        *TEST += 1;
+        delay_ms(50);
+
+        counter += 1;
     }
 }
